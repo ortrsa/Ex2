48904eef72a8973eb4e72dd425b10d4c111da249
Nadav-Eps <nadavepstein1@gmail.com>
2020-11-23 10:52:47 +0200

    naDAV TEST

--- src/api/DWGraph_DS.java
+++ src/api/DWGraph_DS.java
@@ -57,4 +57,51 @@ public class DWGraph_DS implements directed_weighted_graph {
     public int getMC() {
         return 0;
     }
+    private class DWNode implements node_data{
+
+        @Override
+        public int getKey() {
+            return 0;
+        }
+
+        @Override
+        public geo_location getLocation() {
+            return null;
+        }
+
+        @Override
+        public void setLocation(geo_location p) {
+
+        }
+
+        @Override
+        public double getWeight() {
+            return 0;
+        }
+
+        @Override
+        public void setWeight(double w) {
+
+        }
+
+        @Override
+        public String getInfo() {
+            return null;
+        }
+
+        @Override
+        public void setInfo(String s) {
+
+        }
+
+        @Override
+        public int getTag() {
+            return 0;
+        }
+
+        @Override
+        public void setTag(int t) {
+
+        }
+    }
 }
