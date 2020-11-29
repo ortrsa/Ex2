f796ead8933edb073079ab0c187230ab8a9a5282
or trabelsi <ortrsa@gmail.com>
2020-11-29 19:05:04 +0200

    test

--- src/api/DWGraph_Algo.java
+++ src/api/DWGraph_Algo.java
@@ -3,15 +3,16 @@ package api;
 import java.util.*;
 
 public class DWGraph_Algo implements dw_graph_algorithms {
-    private  directed_weighted_graph gr;
+    private directed_weighted_graph gr;
     private HashMap<Integer, Double> nodeDis;
     private HashMap<Integer, node_data> nodePar;
     private Set<Integer> used;
     private PriorityQueue<node_data> unused;
     private List<node_data> path;
+
     @Override
     public void init(directed_weighted_graph g) {
-    this.gr=g;
+        this.gr = g;
     }
 
     @Override
@@ -23,14 +24,14 @@ public class DWGraph_Algo implements dw_graph_algorithms {
     public directed_weighted_graph copy() {
         directed_weighted_graph g = new DWGraph_DS();
         Iterator<node_data> itr = gr.getV().iterator();
-        while(itr.hasNext()){
+        while (itr.hasNext()) {
             node_data TmpNode = new NodeData();
-            TmpNode=itr.next();
+            TmpNode = itr.next();
             g.addNode(TmpNode);
-            Iterator<edge_data>itr1=gr.getE(TmpNode.getKey()).iterator();
-            while(itr1.hasNext()){
-                edge_data e=itr1.next();
-                g.connect(TmpNode.getKey(),e.getSrc(),e.getWeight());
+            Iterator<edge_data> itr1 = gr.getE(TmpNode.getKey()).iterator();
+            while (itr1.hasNext()) {
+                edge_data e = itr1.next();
+                g.connect(TmpNode.getKey(), e.getSrc(), e.getWeight());
             }
         }
         return g;
@@ -39,48 +40,48 @@ public class DWGraph_Algo implements dw_graph_algorithms {
     @Override
     public boolean isConnected() {
 
-    Iterator<node_data> it = gr.getV().iterator();
+        Iterator<node_data> it = gr.getV().iterator();
         if (!it.hasNext()) return true;
-    boolean flag = true;
+        boolean flag = true;
 
 
-    Queue<node_data> Q = new LinkedList<>();
-    Iterator<node_data> it1 = gr.getV().iterator();
+        Queue<node_data> Q = new LinkedList<>();
+        Iterator<node_data> it1 = gr.getV().iterator();
         Q.add(it1.next());
         Q.peek().setTag(0);
 
         while (!Q.isEmpty()) {
-        node_data t3 = Q.poll();
-        Iterator<edge_data> it2 = gr.getE(t3.getKey()).iterator();
-        while (it2.hasNext()) {
-            edge_data e = it2.next();
-            if (gr.getNode(e.getDest()).getTag() < 0) {
-                gr.getNode(e.getDest()).setTag(0);
-                Q.add(gr.getNode(e.getDest()));
+            node_data t3 = Q.poll();
+            Iterator<edge_data> it2 = gr.getE(t3.getKey()).iterator();
+            while (it2.hasNext()) {
+                edge_data e = it2.next();
+                if (gr.getNode(e.getDest()).getTag() < 0) {
+                    gr.getNode(e.getDest()).setTag(0);
+                    Q.add(gr.getNode(e.getDest()));
+
+                }
 
             }
-
         }
-    }
 
 
         while (it.hasNext()) {
-        node_data t8 = it.next();
-        if (t8.getTag() == -1) {
-            flag = false;
+            node_data t8 = it.next();
+            if (t8.getTag() == -1) {
+                flag = false;
+            }
+            t8.setTag(-1);
         }
-        t8.setTag(-1);
-    }
 
         return flag;
-}
+    }
     @Override
     public double shortestPathDist(int src, int dest) {
         if (gr.getNode(src) == null || gr.getNode(dest) == null) return -1;
         used = new HashSet<>();
         unused = new PriorityQueue<>();
         nodeDis = new HashMap<>();
-        
+
         Dijkstra(src);
 
         return gr.getNode(dest).getTag();
@@ -138,6 +139,6 @@ public class DWGraph_Algo implements dw_graph_algorithms {
 
 
     }
-    
-    
+
+
 }
